df8d223b2bc81fde56d6d399f21941ba5c13194a
linshaoyong <shaoyong.lin@baishancloud.com>
2019-07-15 21:23:30 +0800

    0274 done

--- array/0274_h_index_test.py
+++ array/0274_h_index_test.py
@@ -4,12 +4,23 @@ class Solution(object):
         :type citations: List[int]
         :rtype: int
         """
-        return 0
+        n = len(citations)
+        sc = sorted(citations)
+        v = 0
+        for i in range(n - 1, -1, -1):
+            if sc[i] >= n - i:
+                if i == 0:
+                    return n - i
+                if sc[i - 1] <= n - i:
+                    return n - i
+        return v
 
 
 def test_h_index():
     s = Solution()
     assert 3 == s.hIndex([3, 0, 6, 1, 5])
+    assert 3 == s.hIndex([4, 0, 6, 1, 5])
+    assert 4 == s.hIndex([4, 0, 6, 4, 5])
     assert 0 == s.hIndex([])
     assert 0 == s.hIndex([0])
     assert 1 == s.hIndex([1])
