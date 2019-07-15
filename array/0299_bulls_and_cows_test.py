bab9ac1a65c5077e8df119294ca89f6af5c709c1
linshaoyong <shaoyong.lin@baishancloud.com>
2019-07-15 14:41:56 +0800

    0299 done

--- array/0299_bulls_and_cows_test.py
+++ array/0299_bulls_and_cows_test.py
@@ -5,8 +5,24 @@ class Solution(object):
         :type guess: str
         :rtype: str
         """
-        pass
+        b, t = 0, 0
+        hs, hg = {}, {}
+        for i in range(0, len(secret)):
+            sv, gv = secret[i], guess[i]
+            hs[sv] = hs.get(sv, 0) + 1
+            hg[gv] = hg.get(gv, 0) + 1
+            if sv == gv:
+                b += 1
+
+        for k, v in hs.items():
+            t += min(v, hg.get(k, 0))
+        return str(b) + "A" + str(t - b) + "B"
 
 
 def test_get_hint():
     s = Solution()
+
+    assert "1A3B" == s.getHint("1807", "7810")
+    assert "1A1B" == s.getHint("1123", "0111")
+    assert "1A0B" == s.getHint("11", "10")
+    assert "1A1B" == s.getHint("9305", "9012")
