66b0098919d3d511a157bdcee42af134d27a0b13
linshaoyong <linshaoyong@gmail.com>
2019-07-22 22:37:18 +0800

    0168 done

--- string/0168_excel_sheet_column_title.py
+++ string/0168_excel_sheet_column_title.py
@@ -4,13 +4,12 @@ class Solution(object):
         :type n: int
         :rtype: str
         """
-        m = {1: "A", 2: "B", 3: "C", 4: "D", 5: "E", 6: "F",
-             7: "G", 8: "H", 9: "I", 10: "J", 11: "K", 12: "L",
-             13: "M", 14: "N", 15: "O", 16: "P", 17: "Q", 18: "R",
-             19: "S", 20: "T", 21: "U", 22: "V", 23: "W", 24: "X",
-             25: "Y", 26: "Z"}
-        s = []
-        return "".join(s)
+        capitals = [chr(x) for x in range(ord('A'), ord('Z') + 1)]
+        result = []
+        while n > 0:
+            result.append(capitals[(n - 1) % 26])
+            n = (n - 1) // 26
+        return ''.join(result[::-1])
 
 
 def test_convert_to_title():
