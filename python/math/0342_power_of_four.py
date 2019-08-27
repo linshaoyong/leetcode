ae8fd9f2a2aaed9f7b75b3b0a9eb92326a9c12ec
linshaoyong <linshaoyong@gmail.com>
2019-08-27 16:58:09 +0800

    0342 done

--- python/math/0342_power_of_four.py
+++ python/math/0342_power_of_four.py
@@ -4,9 +4,13 @@ class Solution(object):
         :type num: int
         :rtype: bool
         """
+        b = bin(num)
+        return num > 0 and b.count("1") == 1 and \
+            (len(b) - b.index("1")) % 2 == 1
 
 
 def test_is_power_of_four():
     s = Solution()
     assert s.isPowerOfFour(16)
+    assert s.isPowerOfFour(2) is False
     assert s.isPowerOfFour(5) is False
