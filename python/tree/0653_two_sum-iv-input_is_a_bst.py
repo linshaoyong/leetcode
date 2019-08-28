a8cc8e32c1cde86326c2501ebe4a2cfb89645a7b
linshaoyong <linshaoyong@gmail.com>
2019-08-28 15:47:27 +0800

    0653 done

---
+++ python/tree/0653_two_sum-iv-input_is_a_bst.py
@@ -0,0 +1,46 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    def findTarget(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: bool
+        """
+        finded = [False]
+        self.traversal(root, set(), k, finded)
+        return finded[0]
+
+    def traversal(self, node, targets, k, finded):
+        if finded[0]:
+            return
+        if node:
+            if node.val in targets:
+                finded[0] = True
+            else:
+                targets.add(k - node.val)
+                self.traversal(node.left, targets, k, finded)
+                self.traversal(node.right, targets, k, finded)
+
+
+def test_find_target():
+    a = TreeNode(5)
+    b = TreeNode(3)
+    c = TreeNode(6)
+    d = TreeNode(2)
+    e = TreeNode(4)
+    f = TreeNode(7)
+    a.left = b
+    a.right = c
+    b.left = d
+    b.right = e
+    c.right = f
+
+    assert Solution().findTarget(a, 9)
+    assert Solution().findTarget(a, 28) is False
