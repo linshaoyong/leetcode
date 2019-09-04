b7ef2d80fca891c988f13490d2b87927b9f85a6e
linshaoyong <linshaoyong@gmail.com>
2019-09-04 11:30:17 +0800

    0654 done

--- python/tree/0654_maximum_binary_tree.py
+++ python/tree/0654_maximum_binary_tree.py
@@ -12,6 +12,34 @@ class Solution(object):
         :type nums: List[int]
         :rtype: TreeNode
         """
+        if not nums:
+            return None
+        i, v = self.find_max(nums)
+        node = TreeNode(v)
+        self.traversal(node, nums[0:i], nums[i + 1:])
+        return node
+
+    def traversal(self, parent, lns, rns):
+        if parent:
+            if lns:
+                i, v = self.find_max(lns)
+                left = TreeNode(v)
+                parent.left = left
+                self.traversal(left, lns[0:i], lns[i + 1:])
+            if rns:
+                i, v = self.find_max(rns)
+                right = TreeNode(v)
+                parent.right = right
+                self.traversal(right, rns[0:i], rns[i + 1:])
+
+    def find_max(self, nums):
+        if nums:
+            m = (0, nums[0])
+            for i in range(1, len(nums)):
+                if nums[i] > m[1]:
+                    m = (i, nums[i])
+            return m
+        return None
 
 
 def test_construct_maximum_binary_tree():
