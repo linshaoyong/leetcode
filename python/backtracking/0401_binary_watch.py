a5bdacf9367d7c5390e96b058254d6c8e74a10a7
linshaoyong <linshaoyong@gmail.com>
2019-09-06 17:59:05 +0800

    0401 done

--- python/backtracking/0401_binary_watch.py
+++ python/backtracking/0401_binary_watch.py
@@ -4,26 +4,43 @@ class Solution(object):
         :type num: int
         :rtype: List[str]
         """
-        minutes = [1, 2, 4, 8, 16, 32, 60, 120, 240, 480]
+        self.slots = [1, 2, 4, 8, 16, 32, 1, 2, 4, 8]
         res = []
-        self.read([], len(minutes), num, res)
-        print(res)
+        self.read([], 0, len(self.slots), num, res)
+        result = []
+        for indexs in res:
+            v = self.get_time(indexs)
+            if v:
+                result.append(v)
+        return result
 
-    def read(self, readed, minutes, num, res):
+    def read(self, readed, start, end, num, res):
         if num == 0:
             res.append([r for r in readed])
             return
-        for i in range(0, minutes):
-            if i not in readed:
+        for i in range(start, end):
+            if not (readed and i <= readed[-1]):
                 readed.append(i)
-                self.read(readed, minutes, num - 1, res)
-            if readed:
+                self.read(readed, start + 1, end, num - 1, res)
                 readed.pop()
+
+    def get_time(self, indexs):
+        minutes = 0
+        hours = 0
+        for i in indexs:
+            if i >= 6:
+                hours += self.slots[i]
+            else:
+                minutes += self.slots[i]
+        if hours >= 12 or minutes >= 60:
+            return ''
+        return str(hours) + ":0" + str(minutes) if minutes < 10 \
+            else str(hours) + ":" + str(minutes)
 
 
 def test_read_binary_watch():
 
-    r = Solution().readBinaryWatch(5)
+    r = Solution().readBinaryWatch(1)
 
     assert 10 == len(r)
     assert "1:00" in r
@@ -36,3 +53,31 @@ def test_read_binary_watch():
     assert "0:08" in r
     assert "0:16" in r
     assert "0:32" in r
+
+    r = Solution().readBinaryWatch(2)
+    assert 44 == len(r)
+    assert "0:03" in r
+    assert "0:05" in r
+    assert "0:06" in r
+    assert "0:09" in r
+    assert "0:10" in r
+    assert "0:12" in r
+    assert "0:17" in r
+    assert "0:18" in r
+    assert "0:20" in r
+    assert "0:24" in r
+    assert "0:33" in r
+    assert "0:34" in r
+    assert "0:36" in r
+    assert "0:40" in r
+    assert "0:48" in r
+    assert "1:01" in r
+    assert "1:02" in r
+    assert "1:04" in r
+    assert "1:08" in r
+
+    r = Solution().readBinaryWatch(3)
+    assert 112 == len(r)
+
+    r = Solution().readBinaryWatch(4)
+    assert 181 == len(r)
