5fc9a43622e430348cf1828dc7b97fdca54f0133
linshaoyong <linshaoyong@gmail.com>
2019-09-12 15:56:16 +0800

    144 done

---
+++ python/queue_stack/0144_binary_tree_preorder_traversal.py
@@ -0,0 +1,36 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    def preorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        if not root:
+            return []
+        res = []
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            res.append(node.val)
+            if node.right:
+                stack.append(node.right)
+            if node.left:
+                stack.append(node.left)
+        return res
+
+
+def test_preorder_traversal():
+    a = TreeNode(1)
+    b = TreeNode(2)
+    c = TreeNode(3)
+    a.right = b
+    b.left = c
+
+    assert [1, 2, 3] == Solution().preorderTraversal(a)
