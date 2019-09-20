3779db8ac58dc1cfbd1956981d882af8d853aac5
linshaoyong <linshaoyong@gmail.com>
2019-09-20 23:17:21 +0800

    0849 done

--- python/array/0849_maximize_distance_to_closest_person.py
+++ python/array/0849_maximize_distance_to_closest_person.py
@@ -4,9 +4,20 @@ class Solution(object):
         :type seats: List[int]
         :rtype: int
         """
+        start = -1
+        max_zeros = 0
+        for i in range(len(seats)):
+            if seats[i] == 1:
+                if i > 0 and seats[i - 1] == 0:
+                    zeros = 2 * (i - start - 1) if start < 0 else i - start - 1
+                    max_zeros = max(max_zeros, zeros)
+                start = i
+        max_zeros = max(max_zeros, (len(seats) - 1 - start) * 2)
+        return (max_zeros + 1) // 2
 
 
 def test_max_dist_to_closest():
     s = Solution()
     assert 2 == s.maxDistToClosest([1, 0, 0, 0, 1, 0, 1])
     assert 3 == s.maxDistToClosest([1, 0, 0, 0])
+    assert 3 == s.maxDistToClosest([0, 0, 0, 1])
