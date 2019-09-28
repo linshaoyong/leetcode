dc8504e5b5e6c390a31358498809686aae439f44
linshaoyong <linshaoyong@gmail.com>
2019-09-28 23:59:20 +0800

    0394 done

---
+++ python/math/0492_construct_the_rectangle.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def constructRectangle(self, area):
+        """
+        :type area: int
+        :rtype: List[int]
+        """
+
+
+def test_construct_rectangle():
+    s = Solution()
+    assert [2, 2] == s.constructRectangle(4)
