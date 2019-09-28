b9b9931409f74bf33d95298cb82fc1f05191fa97
linshaoyong <linshaoyong@gmail.com>
2019-09-29 00:06:27 +0800

    0492 done

--- python/math/0492_construct_the_rectangle.py
+++ python/math/0492_construct_the_rectangle.py
@@ -1,11 +1,25 @@
+import math
+
+
 class Solution(object):
     def constructRectangle(self, area):
         """
         :type area: int
         :rtype: List[int]
         """
+        m = area
+        res = [area, 1]
+        for i in range(2, int(math.sqrt(area)) + 1):
+            if area % i == 0:
+                weight = i
+                length = area // weight
+                if length - weight < m:
+                    res[0] = length
+                    res[1] = weight
+        return res
 
 
 def test_construct_rectangle():
     s = Solution()
     assert [2, 2] == s.constructRectangle(4)
+    assert [2, 1] == s.constructRectangle(2)
