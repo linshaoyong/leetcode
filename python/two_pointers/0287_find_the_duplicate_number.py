e8e223e514a57d32c9b9c308ca8cbec2db6cab50
linshaoyong <linshaoyong@gmail.com>
2019-09-30 15:57:37 +0800

    0287 done

--- python/two_pointers/0287_find_the_duplicate_number.py
+++ python/two_pointers/0287_find_the_duplicate_number.py
@@ -4,7 +4,19 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        return False
+        start, end = 1, len(nums) - 1
+        while start < end:
+            mid = start + (end - start) // 2
+            counter = 0
+            for num in nums:
+                if num <= mid:
+                    counter += 1
+
+            if counter > mid:
+                end = mid
+            else:
+                start = mid + 1
+        return start
 
 
 def test_find_duplicate():
