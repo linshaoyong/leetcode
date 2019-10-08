541e837a6eb0986b36b367274fb763ea8e204378
linshaoyong <linshaoyong@gmail.com>
2019-10-08 16:23:03 +0800

    0459 done

--- python/string/0459_repeated_substring_pattern.py
+++ python/string/0459_repeated_substring_pattern.py
@@ -4,6 +4,24 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
+        start, end = 0, 0
+        for i in range(1, len(s)):
+            if s[i] != s[0]:
+                end += 1
+            else:
+                if self.repeated(s, end - start + 1):
+                    return True
+                else:
+                    end += 1
+        return False
+
+    def repeated(self, s, k):
+        if len(s) % k != 0:
+            return False
+        for i in range(k, len(s), k):
+            if s[i:i + k] != s[i - k:i]:
+                return False
+        return True
 
 
 def test_repeated_substring_pattern():
