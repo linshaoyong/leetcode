e5c68fc32122a4106f656723cd013d44ee04827e
linshaoyong <linshaoyong@gmail.com>
2019-10-14 10:56:02 +0800

    0994 done

--- python/array/0994_rotting_oranges.py
+++ python/array/0994_rotting_oranges.py
@@ -8,29 +8,32 @@ class Solution(object):
         while True:
             ones, transforms = 0, []
             for i in range(len(grid)):
-                for j in range(len(grid[0])):
+                for j in range(len(grid[i])):
                     if grid[i][j] == 1:
                         ones += 1
-                        if i > 0 and grid[i - 1][j] == 2:
-                            transforms.append((i, j,))
-                            continue
-                        if i < len(grid) - 1 and grid[i + 1][j] == 2:
-                            transforms.append((i, j,))
-                            continue
-                        if j > 0 and grid[i][j - 1] == 2:
-                            transforms.append((i, j,))
-                            continue
-                        if j < len(grid[i]) - 1 and grid[i][j + 1] == 2:
-                            transforms.append((i, j,))
-                            continue
+                    if self.rotting(i, j, grid):
+                        transforms.append((i, j,))
+
             if not transforms:
                 return -1 if ones > 0 else 0
             if ones == len(transforms):
-                r += 1
-                return r
+                return r + 1
             for x, y in transforms:
                 grid[x][y] = 2
             r += 1
+
+    def rotting(self, i, j, grid):
+        if grid[i][j] != 1:
+            return False
+        if i > 0 and grid[i - 1][j] == 2:
+            return True
+        if i < len(grid) - 1 and grid[i + 1][j] == 2:
+            return True
+        if j > 0 and grid[i][j - 1] == 2:
+            return True
+        if j < len(grid[i]) - 1 and grid[i][j + 1] == 2:
+            return True
+        return False
 
 
 def test_oranges_rotting():
