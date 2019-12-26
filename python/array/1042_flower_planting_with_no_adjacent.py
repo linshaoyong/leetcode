5814cb7fbea0ff8bf14190be2fc0951b33610bab
linshaoyong <linshaoyong@gmail.com>
2019-12-26 09:19:33 +0800

    1042 done

--- python/array/1042_flower_planting_with_no_adjacent.py
+++ python/array/1042_flower_planting_with_no_adjacent.py
@@ -1,10 +1,18 @@
 class Solution:
     def gardenNoAdj(self, N, paths):
-        pass
+        res = [0] * N
+        G = [[] for i in range(N)]
+        for x, y in paths:
+            G[x - 1].append(y - 1)
+            G[y - 1].append(x - 1)
+        for i in range(N):
+            res[i] = ({1, 2, 3, 4} - {res[j] for j in G[i]}).pop()
+        return res
 
 
 def test_garden_no_adj():
     s = Solution()
+    assert [1, 1, 1, 1, 1, 1, 1, 1, 1, 1] == s.gardenNoAdj(10, [])
     assert [1, 2, 3] == s.gardenNoAdj(3, [[1, 2], [2, 3], [3, 1]])
     assert [1, 1, 2, 3] == s.gardenNoAdj(4, [[3, 4], [4, 2], [3, 2], [1, 3]])
     assert [1, 2, 1, 3, 3] == s.gardenNoAdj(
