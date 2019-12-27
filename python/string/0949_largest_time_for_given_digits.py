deb3fd8418dad836a589e7cb977bba91d7a6fc04
linshaoyong <linshaoyong@gmail.com>
2019-12-27 09:47:39 +0800

    0949 done

--- python/string/0949_largest_time_for_given_digits.py
+++ python/string/0949_largest_time_for_given_digits.py
@@ -1,9 +1,16 @@
+import itertools
+
+
 class Solution(object):
     def largestTimeFromDigits(self, A):
         """
         :type A: List[int]
         :rtype: str
         """
+        for p in itertools.permutations(sorted(A, reverse=True)):
+            if p[0] * 10 + p[1] < 24 and p[2] < 6:
+                return f"{p[0]}{p[1]}:{p[2]}{p[3]}"
+        return ""
 
 
 def test_largest_time_from_digits():
