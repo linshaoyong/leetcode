5771954cbf0a5858ed70079a87e256d8f09d1500
linshaoyong <linshaoyong@gmail.com>
2020-01-19 20:46:02 +0800

    1049 done

--- python/dynamic_programming/1049_last_stone_weight_ii.py
+++ python/dynamic_programming/1049_last_stone_weight_ii.py
@@ -1,16 +1,14 @@
 class Solution(object):
     def lastStoneWeightII(self, stones):
-        """
-        :type stones: List[int]
-        :rtype: int
-        """
         dp = {0}
+        sumv = sum(stones)
         for stone in stones:
-            dp = {stone + x for x in dp} | {abs(stone - x) for x in dp}
-        return min(dp)
+            dp |= {stone + d for d in dp}
+        return min(abs(sumv - d - d) for d in dp)
 
 
 def test_last_stone_weight_ii():
     s = Solution()
     assert 1 == s.lastStoneWeightII([2, 7, 4, 1, 8, 1])
     assert 5 == s.lastStoneWeightII([31, 26, 33, 21, 40])
+    assert 97 == s.lastStoneWeightII([1, 2, 100])
