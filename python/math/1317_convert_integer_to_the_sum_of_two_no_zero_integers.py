be880c0332bf264f3c4e86eb3a3ea2f135f2665f
linshaoyong <linshaoyong@gmail.com>
2020-01-19 10:38:33 +0800

    1317 done

--- python/math/1317_convert_integer_to_the_sum_of_two_no_zero_integers.py
+++ python/math/1317_convert_integer_to_the_sum_of_two_no_zero_integers.py
@@ -4,8 +4,13 @@ class Solution(object):
         :type n: int
         :rtype: List[int]
         """
+        for i in range(1, n):
+            if '0' not in f'{i}{n-i}':
+                return [i, n - i]
 
 
 def test_get_no_zero_integers():
     s = Solution()
     assert [1, 1] == s.getNoZeroIntegers(2)
+    assert [1, 213] == s.getNoZeroIntegers(214)
+    assert [58, 999] == s.getNoZeroIntegers(1057)
