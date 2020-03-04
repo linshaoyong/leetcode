0eb6a1aee811ae3e83e61c03e47412d6f4902dab
linshaoyong <linshaoyong@gmail.com>
2020-03-04 22:21:42 +0800

    0892 done

--- python/math/0892_surface_area_of_3d_shapes.py
+++ python/math/0892_surface_area_of_3d_shapes.py
@@ -4,6 +4,17 @@ class Solution(object):
         :type grid: List[List[int]]
         :rtype: int
         """
+        res = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                if grid[i][j] == 0:
+                    continue
+                res += 4 * grid[i][j] + 2
+                if j > 0:
+                    res -= min(grid[i][j], grid[i][j - 1]) * 2
+                if i > 0:
+                    res -= min(grid[i][j], grid[i - 1][j]) * 2
+        return res
 
 
 def test_surface_area():
