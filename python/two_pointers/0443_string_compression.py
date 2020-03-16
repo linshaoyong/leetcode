a38f8fd08377f85700d45b3f29cd648a29a315c8
linshaoyong <linshaoyong@gmail.com>
2020-03-17 00:46:30 +0800

    0443 done

--- python/two_pointers/0443_string_compression.py
+++ python/two_pointers/0443_string_compression.py
@@ -4,11 +4,42 @@ class Solution(object):
         :type chars: List[str]
         :rtype: int
         """
+        prev, count, index = chars[0], 1, 0
+        for i in range(1, len(chars)):
+            if chars[i] == prev:
+                count += 1
+            else:
+                index = self.in_place_replace(chars, prev, count, index)
+                prev, count = chars[i], 1
+        index = self.in_place_replace(chars, prev, count, index)
+        return index
+
+    def in_place_replace(self, chars, prev, count, start):
+        chars[start] = prev
+        index = start + 1
+        if count > 1:
+            for n in str(count):
+                chars[index] = n
+                index += 1
+        return index
 
 
 def test_compress():
     s = Solution()
-    assert 6 == s.compress(["a", "a", "b", "b", "c", "c", "c"])
-    assert 1 == s.compress(["a"])
-    assert 4 == s.compress(
-        ["a", "b", "b", "b", "b", "b", "b", "b", "b", "b", "b", "b", "b"])
+
+    chars = ["a", "a", "b", "b", "c", "c", "c"]
+    assert 6 == s.compress(chars)
+    assert ["a", "2", "b", "2", "c", "3", "c"] == chars
+
+    chars = ["a"]
+    assert 1 == s.compress(chars)
+    assert ["a"] == chars
+
+    chars = ["a", "b", "b", "b", "b", "b", "b", "b", "b", "b", "b", "b", "b"]
+    assert 4 == s.compress(chars)
+    assert ["a", "b", "1", "2", "b", "b", "b",
+            "b", "b", "b", "b", "b", "b"] == chars
+
+    chars = ["a", "a", "a", "b", "b", "a", "a"]
+    assert 6 == s.compress(chars)
+    assert ["a", "3", "b", "2", "a", "2", "a"] == chars
