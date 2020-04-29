9c731f1811e73c8743d5a08f3bf9b9dbd53302d0
linshaoyong <linshaoyong@gmail.com>
2020-04-29 23:59:31 +0800

    0018 done

--- python/array/0018_four_sum.py
+++ python/array/0018_four_sum.py
@@ -5,6 +5,19 @@ class Solution(object):
         :type target: int
         :rtype: List[List[int]]
         """
+        res = set()
+        nums.sort()
+        for i, v in enumerate(nums[:-3]):
+            for j, vv in enumerate(nums[i + 1:-2]):
+                t = target - v - vv
+                d = {}
+                for x in nums[i + j + 2:]:
+                    if x not in d:
+                        d[t - x] = 1
+                    else:
+                        res.add((v, vv, t - x, x))
+
+        return [list(i) for i in res]
 
 
 def test_four_sum():
@@ -15,3 +28,6 @@ def test_four_sum():
     assert [-1, 0, 0, 1] in res
     assert [-2, -1, 1, 2] in res
     assert [-2, 0, 0, 2] in res
+
+    res = s.fourSum([-3, -1, 0, 2, 4, 5], 0)
+    assert [[-3, -1, 0, 4]] == res
