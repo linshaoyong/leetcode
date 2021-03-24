124dfa91d0a2851ad253d7efb9948c4605da42cd
linshaoyong <linshaoyong@gmail.com>
2021-03-24 23:19:50 +0800

    1576 done

--- rust/src/easy/e1576_replace_all_qs_to_avoid_consecutive_repeating_characters.rs
+++ rust/src/easy/e1576_replace_all_qs_to_avoid_consecutive_repeating_characters.rs
@@ -1,8 +1,30 @@
+use std::collections::HashSet;
+
 struct Solution;
 
 impl Solution {
     pub fn modify_string(s: String) -> String {
-        "".to_string()
+        let mut cs: Vec<char> = s.chars().collect();
+        let mut excludes: HashSet<char> = HashSet::new();
+        let abc = vec!['a', 'b', 'c'];
+        for i in 0..cs.len() {
+            if cs[i] == '?' {
+                if i > 0 {
+                    excludes.insert(cs[i - 1]);
+                }
+                if i < cs.len() - 1 {
+                    excludes.insert(cs[i + 1]);
+                }
+                for cc in &abc {
+                    if !excludes.contains(&cc) {
+                        cs[i] = *cc;
+                        break;
+                    }
+                }
+                excludes.clear();
+            }
+        }
+        cs.iter().collect()
     }
 }
 
@@ -12,7 +34,7 @@ fn test_modify_string() {
     assert_eq!("ubvaw", Solution::modify_string("ubv?w".to_string()));
     assert_eq!("jaqgacb", Solution::modify_string("j?qg??b".to_string()));
     assert_eq!(
-        "acywaipkja",
+        "abywaipkja",
         Solution::modify_string("??yw?ipkj?".to_string())
     );
 }
