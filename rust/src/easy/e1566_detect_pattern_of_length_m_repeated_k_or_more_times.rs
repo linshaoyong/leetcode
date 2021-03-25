012d1a64550f5559353afb57a9c4c8f86dfe2e46
linshaoyong <linshaoyong@gmail.com>
2021-03-25 08:57:52 +0800

    1566 done

--- rust/src/easy/e1566_detect_pattern_of_length_m_repeated_k_or_more_times.rs
+++ rust/src/easy/e1566_detect_pattern_of_length_m_repeated_k_or_more_times.rs
@@ -2,7 +2,24 @@ struct Solution;
 
 impl Solution {
     pub fn contains_pattern(arr: Vec<i32>, m: i32, k: i32) -> bool {
-        true
+        let mut count = 1;
+        let m = m as usize;
+        for i in 0..m {
+            for j in (i + m..arr.len()).step_by(m) {
+                if j + m > arr.len() {
+                    break;
+                }
+                if &arr[j - m..j] == &arr[j..j + m] {
+                    count += 1;
+                    if count >= k {
+                        return true;
+                    }
+                } else {
+                    count = 1;
+                }
+            }
+        }
+        false
     }
 }
 
@@ -22,4 +39,13 @@ fn test_contains_pattern() {
     );
     assert_eq!(false, Solution::contains_pattern(vec![1, 2, 3, 1, 2], 2, 2));
     assert_eq!(false, Solution::contains_pattern(vec![2, 2, 2, 2], 2, 3));
+    assert_eq!(true, Solution::contains_pattern(vec![2, 2], 1, 2));
+    assert_eq!(
+        true,
+        Solution::contains_pattern(vec![3, 2, 2, 1, 2, 2, 1, 1, 1, 2, 3, 2, 2], 3, 2)
+    );
+    assert_eq!(
+        false,
+        Solution::contains_pattern(vec![2, 1, 2, 1, 1, 2, 1, 2, 2, 1, 2, 2, 2], 1, 4)
+    );
 }
