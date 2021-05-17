db0c0c54ae4ae4557c5a419b772a1be3748c6c59
linshaoyong <linshaoyong@gmail.com>
2021-05-17 09:24:58 +0800

    1859 done

--- rust/src/easy/e1859_sorting_the_sentence.rs
+++ rust/src/easy/e1859_sorting_the_sentence.rs
@@ -2,7 +2,14 @@ struct Solution;
 
 impl Solution {
     pub fn sort_sentence(s: String) -> String {
-        "".to_string()
+        let splits: Vec<&str> = s.split(' ').collect();
+        let mut sentences = vec![""; splits.len()];
+        for split in splits {
+            let bs = split.as_bytes();
+            let i = (bs[bs.len() - 1] - 49) as usize;
+            sentences[i] = std::str::from_utf8(&bs[0..bs.len() - 1]).unwrap();
+        }
+        sentences.join(" ")
     }
 }
 
