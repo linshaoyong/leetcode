fc3d7dc9d8ca43cb2c37806de18a783cf3fe5fb0
linshaoyong <linshaoyong@gmail.com>
2021-05-19 14:12:28 +0800

    1283 done

--- rust/src/medium/mod.rs
+++ rust/src/medium/mod.rs
@@ -2,7 +2,6 @@ mod m0547_number_of_provinces;
 mod m0875_koko_eating_bananas;
 
 mod m1011_capacity_to_ship_packages_within_d_days;
-mod m1201_ugly_number_iii;
 mod m1283_find_the_smallest_divisor_given_a_threshold;
 mod m1482_minimum_number_of_days_to_make_m_bouquets;
 
