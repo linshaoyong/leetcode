f4c317a7e2f65d0365b0b754ff6d80755c8872a9
linshaoyong <linshaoyong@gmail.com>
2021-05-19 10:15:52 +0800

    1760 done

--- rust/src/medium/m1760_minimum_limit_of_balls_in_a_bag.rs
+++ rust/src/medium/m1760_minimum_limit_of_balls_in_a_bag.rs
@@ -2,7 +2,21 @@ struct Solution;
 
 impl Solution {
     pub fn minimum_size(nums: Vec<i32>, max_operations: i32) -> i32 {
-        0
+        let mut left = 1;
+        let mut right = *nums.iter().max().unwrap();
+        while left < right {
+            let mid = left + (right - left) / 2;
+            let mut operations = 0;
+            for num in &nums {
+                operations += (num - 1) / mid;
+            }
+            if operations > max_operations {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        left
     }
 }
 
